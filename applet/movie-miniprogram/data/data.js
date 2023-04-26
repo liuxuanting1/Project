d70a744bb4170ce3d2673f8b491040810884d15f
“msmrmj@163.com” <msmrmj@163.com>
2023-04-26 14:58:01 +0800

    add

---
+++ applet/movie-miniprogram/data/data.js
@@ -0,0 +1,64 @@
+var postList = [
+  {
+    title: "精灵旅社4：变身大冒险",
+    content: "《精灵旅社》系列大结局。一场意外使得精灵家族全员变身。为了避免身体永久互换，他们环游世界去寻找治疗方法，在人间开启了一段疯狂冒险，种种奇遇引发笑料不断。",
+    imgSrc: "/images/lpl.jpg",
+    reading: 102,
+    detail: "还记得之前的那几部都能让观众真切感受到Drac家族的温馨，有的部分特别搞笑，有的段落甚是感人，很多台词都给我留下了深刻的印象。比如: “A Zing only happens once in your life, and you have to cherish it.”; “Human, monster, unicorn, as long as you’re happy.”; “I’ve never said that in my life. Bleh, bleh-bleh. I don’t know where that comes from”. 反观最新的这一部，看完除了心疼Drac和有些生气，其他画面都没给我留下任何深刻印象。制作团队这一次真的是太失败了。建议大家有再多时间也别去看这一部《精灵旅社4》",
+    collection: 92,
+    dateTime: "24小时前",
+    headImgSrc: "/images/lpl.jpg",
+    author: "记忆天堂",
+    date: "2020-11-12",
+    avatar: "/images/avatar/1.png",
+    postId: 0,
+    music: {
+      url: "http://music.163.com/song/media/outer/url?id=1372060183.mp3",
+        title: "空-徐海俏",
+        coverImg: "https://y.gtimg.cn/music/photo_new/T002R300x300M000002sNbWp3royJG_1.jpg?max_age=2592000",
+    }
+},
+
+    {
+        date: "Sep 18 2020",
+        title: "黑客帝国：矩阵重启",
+        imgSrc: "/images/hkdg.jpg",
+        avatar: "/images/avatar/2.png",
+        content: "尼奥（基努·里维斯 Keanu Reeves 饰）和崔妮蒂（凯瑞-安·莫斯 Carrie-Anne Moss 饰）重回矩阵：在这里，一个是矩阵创造出来，周而复始的日常世界，另一个则是隐藏其后，真实的人类世界。为了弄清楚自己所处的“现实”究竟是真是假，尼奥不得不再次做出选择",
+        reading: "112",
+        collection: "96",
+        headImgSrc: "/images/hkdg.jpg",
+        author: "小琳琳",
+        dateTime: "24小时前",
+        detail: "整个调性和前三部曲相差巨大，显得过于玩闹了，而剧情一会又想深刻一会又很娱乐，显得非常割裂。叙事上非常meta，不同层级里的“角色“，一会儿在聊现实，一会儿在聊真实，一会儿在聊matrix里的游戏，一会儿在聊这个电影本身。然而大量意义不大的对白显得神棍又拖沓。此外，通过角色之口对影片中的对本影片隐(明)喻的游戏进行吐槽，对各种桥段吐槽，并不会让这些很烂的表现获得免于被喷豁免权。打斗戏和飙车戏都非常拉胯，属于可以直接跳过的内容。以及整个crew的扮相也太讨好了...当然等字幕出来还得再看一遍.//预告片还是很对味儿的 过去二十年了 不仅电影工业有了翻天覆地的革新 在生命科学计算机技术上我们都有了各种各样新的突破 在这样的新时代下黑客帝国如何展现其世界观魅力有待考察",
+        postId: 1,
+        music: {
+          url: "http://music.163.com/song/media/outer/url?id=1386866050.mp3",
+            title: "哈哈哈",
+            coverImg: "https://y.gtimg.cn/music/photo_new/T002R300x300M000002sNbWp3royJG_1.jpg?max_age=2592000"
+        }
+    },
+    {
+        title: "飞驰的人生",
+        content: "《飞驰人生》应该是韩寒三部曲的第三部。从《后悔无期》到《乘风破浪》再到《飞驰人生》",
+        imgSrc: "/images/jumpfly.png",
+        reading: 96,
+        detail: "《飞驰人生》应该是韩寒三部曲的第三部。从《后悔无期》到《乘风破浪》再到《飞驰人生》，故事是越讲越直白，也越来越贴近大众。关于理想、关于青春永远是韩寒作品的主题。也许生活确实像白开水，需要一些假设的梦想，即使大多数人都不曾为梦想努力过，但我们依然爱看其他人追梦，来给自己带来些许的慰藉。",
+        collection: 26,
+        dateTime: "21小时前",
+        headImgSrc: "/images/jumpfly.png",
+        author: "深白色",
+        date: "Nov 20 2016",
+        avatar: "/images/avatar/5.png",
+        postId: 4,
+        music: {
+          url: "http://music.163.com/song/media/outer/url?id=1344368486.mp3",
+            title: "奉献-韩寒",
+            coverImg: "https://y.gtimg.cn/music/photo_new/T002R300x300M000002sNbWp3royJG_1.jpg?max_age=2592000"
+        }
+    },
+]
+
+export {
+  postList
+}
