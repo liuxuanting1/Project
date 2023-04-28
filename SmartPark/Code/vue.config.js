6edf2fed14dc55027dc6225c1724cb9650002814
“msmrmj@163.com” <msmrmj@163.com>
2023-04-28 09:40:57 +0800

    >

--- SmartPark/Code/vue.config.js
+++ SmartPark/Code/vue.config.js
@@ -3,7 +3,7 @@
  * @Date: 2021-11-28 01:41:11
  * @LastEditTime: 2022-02-25 06:36:06
  * @LastEditors: Please set LastEditors
- * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @Description: 打开koroFileHeader查看配置 
  * @FilePath: \vue3ts\vue3\vue.config.js
  */
 
