7d584565b414286487d10c44c8ed7993f8bbcfe6
“msmrmj@163.com” <msmrmj@163.com>
2023-04-30 13:48:37 +0800

    。

--- XinShengHuo/common/http.api.js
+++ XinShengHuo/common/http.api.js
@@ -1,3 +1,11 @@
+/*
+ * @Description: 
+ * @Version: 2.0
+ * @Author: Liu Xuanting
+ * @Date: 2023-04-29 20:10:36
+ * @LastEditors: Liu Xuanting
+ * @LastEditTime: 2023-04-29 23:55:14
+ */
 // 此处第二个参数vm，就是我们在页面使用的this，你可以通过vm获取vuex等操作，更多内容详见uView对拦截器的介绍部分：
 // https://uviewui.com/js/http.html#%E4%BD%95%E8%B0%93%E8%AF%B7%E6%B1%82%E6%8B%A6%E6%88%AA%EF%BC%9F
 const install = (Vue, vm) => {
