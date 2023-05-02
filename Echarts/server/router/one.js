96b278c74840822ddb80b91717bf0c703c26db3d
“msmrmj@163.com” <msmrmj@163.com>
2023-05-02 18:55:04 +0800

    。

--- Echarts/server/router/one.js
+++
@@ -1,9 +0,0 @@
-let express=require("express");
-let router=express.Router()
-let oneData=require("../mock/one.json")
-
-router.get("/data",(req,res)=>{
-    res.send({msg:"我是one的路由地址",chartData:oneData})
-})
-
-module.exports=router;
